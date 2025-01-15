de5e280b0e2d55e3dc4b35662307bbee2d0d9765
yntedero <yntedero@gmail.com>
2025-01-15 10:57:43 +0100

    feat: animation and sprite for character and movement

--- character.py
+++ character.py
@@ -1,10 +1,44 @@
 import pygame
 import constants
+import math
 
 class Character():
-    def __init__(self, x, y):
+    def __init__(self, x, y, animation_list):
+        self.flip = False
+        self.animation_list = animation_list
+        self.frame_index = 0
+        self.update_time = pygame.time.get_ticks()
+        self.image = animation_list[self.frame_index]
         self.rect = pygame.Rect(0, 0, 32, 32)
         self.rect.center = (x, y)
 
+    def move(self, dx, dy):
+        # flip image for left movement
+        if dx < 0:
+            self.flip = True
+        elif dx > 0:
+            self.flip = False
+        #control diagonal movement
+        if dx != 0 and dy != 0:
+            dx *= math.sqrt(2) / 2
+            dy *= math.sqrt(2) / 2
+
+        self.rect.x += dx
+        self.rect.y += dy
+
+    def update(self):
+        animation_cooldown = 75
+        # handle animation
+        self.image = self.animation_list[self.frame_index]
+        # check if enough time has passed since the last update
+        if pygame.time.get_ticks() - self.update_time > animation_cooldown:
+            self.update_time = pygame.time.get_ticks()
+            self.frame_index += 1
+        # if the animation has run out the reset
+        if self.frame_index >= len(self.animation_list):
+            self.frame_index = 0
+
     def draw(self, surface):
-        pygame.draw.rect(surface, (constants.RED), self.rect)
+        flipped_image = pygame.transform.flip(self.image, self.flip, False)
+        surface.blit(flipped_image, self.rect.topleft)
+        pygame.draw.rect(surface, constants.RED, self.rect, 1)
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pygame
-import constants
 
+import constants
 from character import Character
 
 pygame.init()
@@ -8,20 +8,88 @@ pygame.init()
 screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
 pygame.display.set_caption("Dungeon RPG")
 
-#create player
-player = Character(100, 100)
+# clock for frame rate
+clock = pygame.time.Clock()
 
-#game loop
+# define player movement variables
+move_left = False
+move_right = False
+move_up = False
+move_down = False
+
+
+#help function to scale image
+def scale_image(image, scale):
+    w = image.get_width()
+    h = image.get_height()
+    return pygame.transform.scale(image, (w * scale, h * scale))
+
+# create player
+animation_list = []
+for i in range(4):
+    img = pygame.image.load(f"assets/components/charater/idle/{i}.png").convert_alpha()
+    img = scale_image(img, constants.SCALE)
+    animation_list.append(img)
+
+player = Character(100, 100, animation_list)
+
+# game loop
 run = True
 while True:
 
-    #draw player layer
+    # frame rate
+    clock.tick(constants.FPS)
+
+    # draw background
+    screen.fill(constants.BG)
+
+    # calc player movement
+    dx = 0
+    dy = 0
+    if move_right == True:
+        dx = constants.SPEED
+    if move_left == True:
+        dx = -constants.SPEED
+    if move_up == True:
+        dy = -constants.SPEED
+    if move_down == True:
+        dy = constants.SPEED
+
+    # move player
+    player.move(dx, dy)
+
+    # update player animation
+    player.update()
+
+    # draw player layer
     player.draw(screen)
 
-    #even handler
+    # even handler
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
             pygame.quit()
+
+        # keyboard input press
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_a:
+                move_left = True
+            if event.key == pygame.K_d:
+                move_right = True
+            if event.key == pygame.K_w:
+                move_up = True
+            if event.key == pygame.K_s:
+                move_down = True
+
+        # keyboard input release
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_a:
+                move_left = False
+            if event.key == pygame.K_d:
+                move_right = False
+            if event.key == pygame.K_w:
+                move_up = False
+            if event.key == pygame.K_s:
+                move_down = False
 
     pygame.display.update()
