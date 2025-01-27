bff336e293b300dc8668474d18f59da328b1d456
yntedero <yntedero@gmail.com>
2025-01-27 01:04:42 +0100

    style: prettier code reformat lines, docs

--- items.py
+++ items.py
@@ -1,9 +1,9 @@
 import pygame
 
 class Item(pygame.sprite.Sprite):
-  def __init__(self, x, y, item_type, animation_list, dummy_coin = False):
+  def __init__(self, x, y, item_type, animation_list, dummy_coin=False):
     pygame.sprite.Sprite.__init__(self)
-    self.item_type = item_type#0: coin, 1: health potion
+    self.item_type = item_type  # 0: coin, 1: health potion
     self.animation_list = animation_list
     self.frame_index = 0
     self.update_time = pygame.time.get_ticks()
@@ -13,15 +13,13 @@ class Item(pygame.sprite.Sprite):
     self.dummy_coin = dummy_coin
 
   def update(self, screen_scroll, player):
-    #doesn't apply to the dummy coin that is always displayed at the top of the screen
+    # ignore screen scroll for the dummy coin
     if not self.dummy_coin:
-      #reposition based on screen scroll
       self.rect.x += screen_scroll[0]
       self.rect.y += screen_scroll[1]
 
-    #check to see if item has been collected by the player
+    # check if item is collected by the player
     if self.rect.colliderect(player.rect):
-      #coin collected
       if self.item_type == 0:
         player.score += 1
       elif self.item_type == 1:
@@ -30,18 +28,15 @@ class Item(pygame.sprite.Sprite):
           player.health = 100
       self.kill()
 
-    #handle animation
+    # handle animation
     animation_cooldown = 150
-    #update image
     self.image = self.animation_list[self.frame_index]
-    #check if enough time has passed since the last update
     if pygame.time.get_ticks() - self.update_time > animation_cooldown:
       self.frame_index += 1
       self.update_time = pygame.time.get_ticks()
-    #check if the animation has finished
     if self.frame_index >= len(self.animation_list):
       self.frame_index = 0
 
-
+  # drawing logic
   def draw(self, surface):
     surface.blit(self.image, self.rect)
