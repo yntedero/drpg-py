8462d44859f9963b93cbf4e299bb1189cc083962
yntedero <yntedero@gmail.com>
2025-01-27 01:08:35 +0100

    fix: fix ai bad movement, complete level checker, range of fireball, docs and restyling code

--- character.py
+++ character.py
@@ -11,7 +11,7 @@ class Character():
     self.flip = False
     self.animation_list = mob_animations[char_type]
     self.frame_index = 0
-    self.action = 0#0:idle, 1:run
+    self.action = 0  # 0:idle, 1:run
     self.update_time = pygame.time.get_ticks()
     self.running = False
     self.health = health
@@ -25,47 +25,56 @@ class Character():
     self.rect = pygame.Rect(0, 0, constants.TILE_SIZE * size, constants.TILE_SIZE * size)
     self.rect.center = (x, y)
 
-  def move(self, dx, dy, obstacle_tiles):
+  # movement and collision logic
+  def move(self, dx, dy, obstacle_tiles, exit_tile=None):
     screen_scroll = [0, 0]
+    level_complete = False
     self.running = False
-    
+
     if dx != 0 or dy != 0:
       self.running = True
     if dx < 0:
       self.flip = True
     if dx > 0:
       self.flip = False
-    #control diagonal speed
-    if dx != 0 and dy !=0:
-      dx = dx * (math.sqrt(2)/2)
-      dy = dy * (math.sqrt(2)/2)
 
-    #check for collision with map in x direction
+    # control diagonal speed
+    if dx != 0 and dy != 0:
+      dx = dx * (math.sqrt(2) / 2)
+      dy = dy * (math.sqrt(2) / 2)
+
+    # check collision with map in x direction
     self.rect.x += dx
     for obstacle in obstacle_tiles:
-      #check for collision
       if obstacle[1].colliderect(self.rect):
-        #check which side the collision is from
         if dx > 0:
           self.rect.right = obstacle[1].left
         if dx < 0:
           self.rect.left = obstacle[1].right
 
-    #check for collision with map in y direction
+    # check collision with map in y direction
     self.rect.y += dy
     for obstacle in obstacle_tiles:
-      #check for collision
       if obstacle[1].colliderect(self.rect):
-        #check which side the collision is from
         if dy > 0:
           self.rect.bottom = obstacle[1].top
         if dy < 0:
           self.rect.top = obstacle[1].bottom
 
-    #logic only applicable to player
+    # logic only for player
     if self.char_type == 0:
-      #update scroll based on player position
-      #move camera left and right
+      # check collision with exit ladder
+      if exit_tile[1].colliderect(self.rect):
+        # ensure player is close to exit center
+        exit_dist = math.sqrt(
+          ((self.rect.centerx - exit_tile[1].centerx) ** 2)
+          + ((self.rect.centery - exit_tile[1].centery) ** 2)
+        )
+        if exit_dist < 20:
+          level_complete = True
+
+      # update scroll based on player position
+      # move camera left and right
       if self.rect.right > (constants.SCREEN_WIDTH - constants.SCROLL_THRESH):
         screen_scroll[0] = (constants.SCREEN_WIDTH - constants.SCROLL_THRESH) - self.rect.right
         self.rect.right = constants.SCREEN_WIDTH - constants.SCROLL_THRESH
@@ -73,7 +82,7 @@ class Character():
         screen_scroll[0] = constants.SCROLL_THRESH - self.rect.left
         self.rect.left = constants.SCROLL_THRESH
 
-      #move camera up and down
+      # move camera up and down
       if self.rect.bottom > (constants.SCREEN_HEIGHT - constants.SCROLL_THRESH):
         screen_scroll[1] = (constants.SCREEN_HEIGHT - constants.SCROLL_THRESH) - self.rect.bottom
         self.rect.bottom = constants.SCREEN_HEIGHT - constants.SCROLL_THRESH
@@ -81,8 +90,9 @@ class Character():
         screen_scroll[1] = constants.SCROLL_THRESH - self.rect.top
         self.rect.top = constants.SCROLL_THRESH
 
-    return screen_scroll
+    return screen_scroll, level_complete
 
+  # ai logic
   def ai(self, player, obstacle_tiles, screen_scroll, fireball_image):
     clipped_line = ()
     stun_cooldown = 100
@@ -90,18 +100,17 @@ class Character():
     ai_dy = 0
     fireball = None
 
-    #reposition the mobs based on screen scroll
+    # reposition mobs based on screen scroll
     self.rect.x += screen_scroll[0]
     self.rect.y += screen_scroll[1]
 
-    #create a line of sight from the enemy to the player
+    # create line of sight from enemy to player
     line_of_sight = ((self.rect.centerx, self.rect.centery), (player.rect.centerx, player.rect.centery))
-    #check if line of sight passes through an obstacle tile
     for obstacle in obstacle_tiles:
       if obstacle[1].clipline(line_of_sight):
         clipped_line = obstacle[1].clipline(line_of_sight)
 
-    #check distance to player
+    # check distance to player
     dist = math.sqrt(((self.rect.centerx - player.rect.centerx) ** 2) + ((self.rect.centery - player.rect.centery) ** 2))
     if not clipped_line and dist > constants.RANGE:
       if self.rect.centerx > player.rect.centerx:
@@ -115,23 +124,30 @@ class Character():
 
     if self.alive:
       if not self.stunned:
-        #move towards player
+        # move towards player
         self.move(ai_dx, ai_dy, obstacle_tiles)
-        #attack player
+        # attack player
         if dist < constants.ATTACK_RANGE and player.hit == False:
           player.health -= 10
           player.hit = True
           player.last_hit = pygame.time.get_ticks()
-        #boss enemies shoot fireballs
+
+        # boss enemies shoot fireballs
         fireball_cooldown = 700
         if self.boss:
           if dist < 500:
             if pygame.time.get_ticks() - self.last_attack >= fireball_cooldown:
-              fireball = weapon.Fireball(fireball_image, self.rect.centerx, self.rect.centery, player.rect.centerx, player.rect.centery)
+              fireball = weapon.Fireball(
+                fireball_image,
+                self.rect.centerx,
+                self.rect.centery,
+                player.rect.centerx,
+                player.rect.centery
+              )
               self.last_attack = pygame.time.get_ticks()
 
-      #check if hit
-      if self.hit == True:
+      # check if hit
+      if self.hit:
         self.hit = False
         self.last_hit = pygame.time.get_ticks()
         self.stunned = True
@@ -143,45 +159,41 @@ class Character():
 
     return fireball
 
+  # update logic
   def update(self):
-    #check if character has died
+    # check if character has died
     if self.health <= 0:
       self.health = 0
       self.alive = False
 
-    #timer to reset player taking a hit
+    # timer to reset player taking a hit
     hit_cooldown = 1000
     if self.char_type == 0:
       if self.hit == True and (pygame.time.get_ticks() - self.last_hit) > hit_cooldown:
         self.hit = False
 
-    #check what action the player is performing
-    if self.running == True:
-      self.update_action(1)#1:run
+    # check what action the player is performing
+    if self.running:
+      self.update_action(1)  # 1:run
     else:
-      self.update_action(0)#0:idle
+      self.update_action(0)  # 0:idle
 
+    # handle animation
     animation_cooldown = 70
-    #handle animation
-    #update image
     self.image = self.animation_list[self.action][self.frame_index]
-    #check if enough time has passed since the last update
     if pygame.time.get_ticks() - self.update_time > animation_cooldown:
       self.frame_index += 1
       self.update_time = pygame.time.get_ticks()
-    #check if the animation has finished
     if self.frame_index >= len(self.animation_list[self.action]):
       self.frame_index = 0
 
   def update_action(self, new_action):
-    #check if the new action is different to the previous one
     if new_action != self.action:
       self.action = new_action
-      #update the animation settings
       self.frame_index = 0
       self.update_time = pygame.time.get_ticks()
 
-
+  # drawing logic
   def draw(self, surface):
     flipped_image = pygame.transform.flip(self.image, self.flip, False)
     if self.char_type == 0:
--- weapon.py
+++ weapon.py
@@ -21,25 +21,33 @@ class Weapon():
 
     pos = pygame.mouse.get_pos()
     x_dist = pos[0] - self.rect.centerx
-    y_dist = -(pos[1] - self.rect.centery)#-ve because pygame y coordinates increase down the screen
+    y_dist = -(pos[1] - self.rect.centery)  # invert because of pygame y-axis
     self.angle = math.degrees(math.atan2(y_dist, x_dist))
 
-    #get mouseclick
-    if pygame.mouse.get_pressed()[0] and self.fired == False and (pygame.time.get_ticks() - self.last_shot) >= shot_cooldown:
+    # check mouse click
+    if pygame.mouse.get_pressed()[0] and not self.fired and (pygame.time.get_ticks() - self.last_shot) >= shot_cooldown:
       arrow = Arrow(self.arrow_image, self.rect.centerx, self.rect.centery, self.angle)
       self.fired = True
       self.last_shot = pygame.time.get_ticks()
-    #reset mouseclick
-    if pygame.mouse.get_pressed()[0] == False:
+
+    # reset mouse click
+    if not pygame.mouse.get_pressed()[0]:
       self.fired = False
 
     return arrow
 
+  # draw weapon
   def draw(self, surface):
     self.image = pygame.transform.rotate(self.original_image, self.angle)
-    surface.blit(self.image, ((self.rect.centerx - int(self.image.get_width()/2)), self.rect.centery - int(self.image.get_height()/2)))
+    surface.blit(
+      self.image,
+      (
+        self.rect.centerx - int(self.image.get_width() / 2),
+        self.rect.centery - int(self.image.get_height() / 2),
+      ),
+    )
 
-
+# arrow logic
 class Arrow(pygame.sprite.Sprite):
   def __init__(self, image, x, y, angle):
     pygame.sprite.Sprite.__init__(self)
@@ -48,29 +56,32 @@ class Arrow(pygame.sprite.Sprite):
     self.image = pygame.transform.rotate(self.original_image, self.angle - 90)
     self.rect = self.image.get_rect()
     self.rect.center = (x, y)
-    #calculate the horizontal and vertical speeds based on the angle
     self.dx = math.cos(math.radians(self.angle)) * constants.ARROW_SPEED
-    self.dy = -(math.sin(math.radians(self.angle)) * constants.ARROW_SPEED)#-ve because pygame y coordinate increases down the screen
+    self.dy = -(math.sin(math.radians(self.angle)) * constants.ARROW_SPEED)
 
-  def update(self,screen_scroll, obstacle_tiles, enemy_list):
-    #reset variables
+  def update(self, screen_scroll, obstacle_tiles, enemy_list):
     damage = 0
     damage_pos = None
 
-    #reposition based on speed
+    # reposition arrow
     self.rect.x += screen_scroll[0] + self.dx
     self.rect.y += screen_scroll[1] + self.dy
 
-    #check for collision between arrow and tile walls
+    # check collision with map
     for obstacle in obstacle_tiles:
       if obstacle[1].colliderect(self.rect):
         self.kill()
 
-    #check if arrow has gone off screen
-    if self.rect.right < 0 or self.rect.left > constants.SCREEN_WIDTH or self.rect.bottom < 0 or self.rect.top > constants.SCREEN_HEIGHT:
+    # check if arrow goes off screen
+    if (
+            self.rect.right < 0
+            or self.rect.left > constants.SCREEN_WIDTH
+            or self.rect.bottom < 0
+            or self.rect.top > constants.SCREEN_HEIGHT
+    ):
       self.kill()
 
-    #check collision between arrow and enemies
+    # check collision with enemies
     for enemy in enemy_list:
       if enemy.rect.colliderect(self.rect) and enemy.alive:
         damage = 10 + random.randint(-5, 5)
@@ -83,10 +94,15 @@ class Arrow(pygame.sprite.Sprite):
     return damage, damage_pos
 
   def draw(self, surface):
-    surface.blit(self.image, ((self.rect.centerx - int(self.image.get_width()/2)), self.rect.centery - int(self.image.get_height()/2)))
+    surface.blit(
+      self.image,
+      (
+        self.rect.centerx - int(self.image.get_width() / 2),
+        self.rect.centery - int(self.image.get_height() / 2),
+      ),
+    )
 
-
-
+# fireball logic
 class Fireball(pygame.sprite.Sprite):
   def __init__(self, image, x, y, target_x, target_y):
     pygame.sprite.Sprite.__init__(self)
@@ -97,27 +113,35 @@ class Fireball(pygame.sprite.Sprite):
     self.image = pygame.transform.rotate(self.original_image, self.angle - 90)
     self.rect = self.image.get_rect()
     self.rect.center = (x, y)
-    #calculate the horizontal and vertical speeds based on the angle
     self.dx = math.cos(math.radians(self.angle)) * constants.FIREBALL_SPEED
-    self.dy = -(math.sin(math.radians(self.angle)) * constants.FIREBALL_SPEED)#-ve because pygame y coordiate increases down the screen
-
+    self.dy = -(math.sin(math.radians(self.angle)) * constants.FIREBALL_SPEED)
 
   def update(self, screen_scroll, player):
-    #reposition based on speed
+    # reposition fireball
     self.rect.x += screen_scroll[0] + self.dx
     self.rect.y += screen_scroll[1] + self.dy
 
-    #check if fireball has gone off screen
-    if self.rect.right < 0 or self.rect.left > constants.SCREEN_WIDTH or self.rect.bottom < 0 or self.rect.top > constants.SCREEN_HEIGHT:
+    # check if fireball goes off screen
+    if (
+            self.rect.right < 0
+            or self.rect.left > constants.SCREEN_WIDTH
+            or self.rect.bottom < 0
+            or self.rect.top > constants.SCREEN_HEIGHT
+    ):
       self.kill()
 
-    #check collision between self and player
-    if player.rect.colliderect(self.rect) and player.hit == False:
+    # check collision with player
+    if player.rect.colliderect(self.rect) and not player.hit:
       player.hit = True
       player.last_hit = pygame.time.get_ticks()
       player.health -= 10
       self.kill()
 
-
   def draw(self, surface):
-    surface.blit(self.image, ((self.rect.centerx - int(self.image.get_width()/2)), self.rect.centery - int(self.image.get_height()/2)))
+    surface.blit(
+      self.image,
+      (
+        self.rect.centerx - int(self.image.get_width() / 2),
+        self.rect.centery - int(self.image.get_height() / 2),
+      ),
+    )
